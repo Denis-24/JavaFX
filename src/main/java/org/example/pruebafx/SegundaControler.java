defe7b7af3e9dd91a0ff14200bafc03de7ebf188
Denis Rico. <ricorenzdenis@gmail.com>
2026-04-23 19:38:01 +0200

    no se

--- src/main/java/org/example/pruebafx/SegundaControler.java
+++ src/main/java/org/example/pruebafx/SegundaControler.java
@@ -34,8 +34,6 @@ public class SegundaControler {
 
     @FXML
     private void initialize(){
-
-
         nameTable.setCellValueFactory(datos -> new SimpleStringProperty(datos.getValue().getNombre()));
         edadTable.setCellValueFactory(datos-> new SimpleIntegerProperty(datos.getValue().getEdad()).asObject());
         personasTableVew.setItems(DatsoPersonas.getListaPersona());
